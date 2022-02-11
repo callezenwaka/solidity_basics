b72e1a246ba44ee4df2316d018903f479aec67c2
"callezenwaka" <"callisezenwaka@gmail.com">
2022-02-11 16:30:14 +0100

    commit deploy contract

---
+++ contracts/DeclareFunction.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract DeclareFunction {
+    uint value;
+    function getValue() external view returns(uint) {
+        return value;
+    }
+
+    function setValue(uint _value) external {
+        value = _value;
+    }
+}
